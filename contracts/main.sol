bf4bb15c51c909ee7270fb096586c1a0bb08716e
himanshu-Bhatt <86202656+himanshu-Bhatt@users.noreply.github.com>
2022-03-02 10:45:21 +0530

    Update main.sol
    its still incomlete

--- contracts/main.sol
+++ contracts/main.sol
@@ -27,9 +27,12 @@ contract Child is ERC721URIStorage {
     // mapping(uint=>(mapping(address=>uint))) totalTicketOwn;
     mapping(uint=>mapping(address=>bool)) haveTicket;
     // mapping(address=>uint) eventOrganiserId;
+    mapping(uint=>mapping(address=>uint)) ticketLevel;
     mapping(uint=>address) eventOrganiserAddress;
     mapping(uint=>uint) eventTicketPrice;
     mapping(uint=>uint) eventEndTime;
+    mapping(uint=>uint) eventMoneyCollection;
+    mapping(uint=>bool) canExtractEventMoney;
     
     string svgPartOne =
         '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
@@ -62,6 +65,14 @@ contract Child is ERC721URIStorage {
         require(totalEventTicket[_eventId]>0,"All tickets are sold");
         require(block.timestamp <= eventEndTime[_eventId],"Deadline to buy this ticket is passed");
 
+        if(msg.value>=(1.6 * eventTicketPrice[_eventId])){
+            ticketLevel[_eventId][msg.sender]=3;
+        }else if(msg.value>=(1.3 * eventTicketPrice[_eventId])){
+            ticketLevel[_eventId][msg.sender]=2;
+        }else{
+            ticketLevel[_eventId][msg.sender]=1;
+        }
+
         uint currentId=_tokenIds.current();
         string memory finalSvg=string(abi.encodePacked(svgPartOne,_name,".",Strings.toString(_eventId),"#",Strings.toString(currentId),svgPartTwo));
         
@@ -82,6 +93,7 @@ contract Child is ERC721URIStorage {
         // );
         totalEventTicket[_eventId]-=1;
         haveTicket[_eventId][msg.sender]=true;
+        eventMoneyCollection[_eventId]+=msg.value;
         _safeMint(msg.sender,currentId);
 
         _tokenIds.increment();
@@ -89,4 +101,10 @@ contract Child is ERC721URIStorage {
     function getEventOrganiserAddress(uint _eventId) external view returns(address){
         return eventOrganiserAddress[_eventId];
     }
+    function getTicketLevel(uint _eventId,address addr) external view returns(bool,uint){
+        bool ownTicket=haveTicket[_eventId][addr];
+        uint ticketLevel=ticketLevel[_eventId][addr];
+        returns(ownTicket,ticketLevel);
+    }
+
 }
